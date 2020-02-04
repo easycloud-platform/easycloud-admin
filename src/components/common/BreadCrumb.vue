5327cbbcf37dec9500f2c93dbfc986164b1358b1
liqian <51103942@qq.com>
2020-02-04 16:20:28 +0800

    完善

--- src/components/common/BreadCrumb.vue
+++ src/components/common/BreadCrumb.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="breadCrumb-wrp" >
-    <span class="text-bold">当前位置： </span>
+
     <el-breadcrumb  class="text-bold" style="display: inline-block; vertical-align: middle;" separator-class="el-icon-arrow-right">
       <!--普通面包屑的处理-->
       <el-breadcrumb-item  v-for="(item,index) in breadcrumbPathName" :key="index" :to="{'name':item}">
@@ -39,9 +39,6 @@
        },
        breadcrumbPathName () {
        	 return store.state.breadcrumbPathName
-       },
-       breadcrumbFlag () {
-       	 return store.state.breadcrumbFlag
        }
     },
     watch: {
@@ -54,7 +51,6 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  /*@import url(../assets/css/test.css);*/
   .breadCrumb-wrp {
     font-size: 0.11rem;
     position: relative;
@@ -66,7 +62,6 @@
   }
 
   .text-bold {
-    font-size: 0.11rem;
     float: left;
     color: rgb(153,153,153);
     line-height: 1;
---
+++ src/components/page/page.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="page">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :page-size="reqData.pageSize"
+        @current-change="handleCurrentChange"
+        :total="total"
+      ></el-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+  import router from '../../router/index'
+  import * as api from '../../axios/api'
+  export default {
+    name: 'page',
+    data () {
+      return {
+       list: [],
+            treeList: [],
+            reqData: {
+              pageNum: 1,
+              pageSize: 4,
+              title: "",
+            },
+            total: 100
+       }
+    },
+    methods: {
+
+    },
+    created () {
+
+    },
+    mounted () {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
